e3c50de59f9f4514161f7894a5d88da1fa1003d7
ThatcherReidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-03-06 15:14:28 +0000

    hello

--- program.py
+++ program.py
@@ -2,4 +2,8 @@
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
 def myFunction():
-  # My Function
+    print("Hello World!")
+
+myFunction()  # Output: "Hello World!"
+
+
